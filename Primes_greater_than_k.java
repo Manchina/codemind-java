1a3a22e0cc036024647f5c079d0a8bde16fb1765
Manchina <104674915+Manchina@users.noreply.github.com>
2022-11-30 19:54:33 +0530

    import java.util.Scanner;
    class PGTK {
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            int n = scanner.nextInt();
            int[] arr = new int[n];
            int count = 0;
            for(int i=0; i<n; i++){
                arr[i] = scanner.nextInt();
            }
            int k = scanner.nextInt();
    
            for(int i=0; i<n; i++){
                if(isPrime(arr[i])){
                    if(arr[i] >= k){
                        count++;
                    }
                }
            }
    
            System.out.printf("%d",count);
    
        }
    
        private static boolean isPrime(int n){
            int c = 0;
            if(n == 1){
                return true;
            }
            for(int i=1; i<n; i++){
                if(n % i == 0){
                    c++;
                }
            }
            if(c == 1){
                return true;
            }
            return false;
        }
    }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class PGTK {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        int count = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        int k = scanner.nextInt();
+
+        for(int i=0; i<n; i++){
+            if(isPrime(arr[i])){
+                if(arr[i] >= k){
+                    count++;
+                }
+            }
+        }
+
+        System.out.printf("%d",count);
+
+    }
+
+    private static boolean isPrime(int n){
+        int c = 0;
+        if(n == 1){
+            return true;
+        }
+        for(int i=1; i<n; i++){
+            if(n % i == 0){
+                c++;
+            }
+        }
+        if(c == 1){
+            return true;
+        }
+        return false;
+    }
+}
