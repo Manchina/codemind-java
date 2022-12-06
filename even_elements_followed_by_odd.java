3118cd9900bbc996dcb660b0133eebbc5d152e7e
Manchina <104674915+Manchina@users.noreply.github.com>
2022-12-06 09:27:40 +0530

    import java.util.*; class EvenFollowOdd{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int[] arr = new int[n];
            for(int i=0; i<n; i++){
                arr[i] = sc.nextInt();
            }
    
            int[] output = new int[n];
            int j = 0;
    
            for(int i=0; i<n; i++){
                if(arr[i] % 2 == 0){
                    output[j] = arr[i];
                    j++;
                }
            }
    
            for(int i=0; i<n; i++){
    
    
                if(arr[i] % 2 != 0){
                    output[j] = arr[i];
                    j++;
                }
            }
    
            for(int i=0; i<n; i++){
                System.out.printf("%d ",output[i]);
            }
        }
    }

---
+++ even_elements_followed_by_odd.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class EvenFollowOdd{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int[] output = new int[n];
+        int j = 0;
+
+        for(int i=0; i<n; i++){
+            if(arr[i] % 2 == 0){
+                output[j] = arr[i];
+                j++;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+
+
+            if(arr[i] % 2 != 0){
+                output[j] = arr[i];
+                j++;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.printf("%d ",output[i]);
+        }
+    }
+}
