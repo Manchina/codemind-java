4390bd6d9b8027e820a5f1f5ae304901c34ff469
Manchina <104674915+Manchina@users.noreply.github.com>
2022-12-06 08:36:06 +0530

    import java.util.*; class Solution{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int[] arr = new int[n];
            for(int i=0; i<n; i++){
                arr[i] = sc.nextInt();
            }
            
            int a = sc.nextInt();
            int b = sc.nextInt();
            int sum = 0;
            
            for(int i=0; i<n; i++){
                if(arr[i]>=a && arr[i]<=b){
                    continue;
                }else{
                    sum += arr[i];
                }
            }
            
            System.out.printf("%d",sum);
        }
    }

---
+++ sum_of_elements_not_between_A_and_B.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+        
+        for(int i=0; i<n; i++){
+            if(arr[i]>=a && arr[i]<=b){
+                continue;
+            }else{
+                sum += arr[i];
+            }
+        }
+        
+        System.out.printf("%d",sum);
+    }
+}
