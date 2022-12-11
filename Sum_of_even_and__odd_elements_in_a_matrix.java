360ea238f2b6637c508c4f034ae9a0c4a00325e2
Manchina <104674915+Manchina@users.noreply.github.com>
2022-12-11 16:32:16 +0530

    import java.util.Scanner;
    class SumOfMatrix {
        public static void main(String[] args) {
            Scanner scanner  = new Scanner(System.in);
            int row = scanner.nextInt();
            int col = scanner.nextInt();
            int[][] arr = new int[row][col];
            int oddSum = 0;
            int evenSum = 0;
    
            for(int i=0; i<row; i++){
                for(int j=0; j<arr[i].length; j++){
                    arr[i][j] = scanner.nextInt();
                }
            }
    
            for(int i=0; i<row; i++){
                for(int j=0; j<arr[i].length; j++){
                   if(arr[i][j]%2==0){
                       evenSum += arr[i][j];
                   }else{
                       oddSum += arr[i][j];
                   }
                }
            }
    
            System.out.printf("%d %d",evenSum,oddSum);
        }
    }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class SumOfMatrix {
+    public static void main(String[] args) {
+        Scanner scanner  = new Scanner(System.in);
+        int row = scanner.nextInt();
+        int col = scanner.nextInt();
+        int[][] arr = new int[row][col];
+        int oddSum = 0;
+        int evenSum = 0;
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<arr[i].length; j++){
+                arr[i][j] = scanner.nextInt();
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<arr[i].length; j++){
+               if(arr[i][j]%2==0){
+                   evenSum += arr[i][j];
+               }else{
+                   oddSum += arr[i][j];
+               }
+            }
+        }
+
+        System.out.printf("%d %d",evenSum,oddSum);
+    }
+}
