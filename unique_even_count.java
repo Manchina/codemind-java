485dbe69d33e90bfa9b02f240af9096a38b3f990
Manchina <104674915+Manchina@users.noreply.github.com>
2022-12-11 11:20:42 +0530

    import java.util.ArrayList; import java.util.List; import java.util.Scanner;
    class UniqueOddElements{
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            List<Integer> list = new ArrayList<Integer>();
            int n = scanner.nextInt();
            int[] arr = new int[n];
            int c = 0;
    
            for (int i=0; i<n; i++){
                arr[i] = scanner.nextInt();
                if(arr[i]%2==0 && !list.contains(arr[i])){
                   list.add(arr[i]);
                   c++;
                }
            }
    
            System.out.printf("%d",c);
    
        }
    }

---
+++ unique_even_count.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class UniqueOddElements{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> list = new ArrayList<Integer>();
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        int c = 0;
+
+        for (int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+            if(arr[i]%2==0 && !list.contains(arr[i])){
+               list.add(arr[i]);
+               c++;
+            }
+        }
+
+        System.out.printf("%d",c);
+
+    }
+}
