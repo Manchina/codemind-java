832257acfbb1545de329eafbf7603cc7ba5a643d
Manchina <104674915+Manchina@users.noreply.github.com>
2022-12-11 15:50:07 +0530

    import java.util.Scanner;
    class PrimesLessThanK {
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
            int n = scanner.nextInt();
            int[] arr = new int[n];
    
            for(int i=0; i<n; i++){
                arr[i] = scanner.nextInt();
            }
    
            int k = scanner.nextInt();
            int c = 0;
    
            for(int i=0; i<n; i++){
                if(isPrime(arr[i]) && arr[i]<=k){
                    c++;
                }
            }
    
            System.out.printf("%d",c);
        }
    
        public static boolean isPrime(int num){
            if(num==1){
                return false;
            }
            int c = 0;
            for(int i=1; i<num; i++){
                if(num%i==0){
                    c++;
                }
            }
            if(c<2){
                return true;
            }
    
            return false;
        }
    }

--- primes_less_than_k.java
+++ primes_less_than_k.java
@@ -1,41 +1,41 @@
 import java.util.Scanner;
 
-class PGTK {
+class PrimesLessThanK {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] arr = new int[n];
-        int count = 0;
+
         for(int i=0; i<n; i++){
             arr[i] = scanner.nextInt();
         }
+
         int k = scanner.nextInt();
+        int c = 0;
 
         for(int i=0; i<n; i++){
-            if(isPrime(arr[i])){
-                if(arr[i] <= k){
-                    count++;
-                }
-            }
-        }
-
-        System.out.printf("%d",count);
-
-    }
-
-    private static boolean isPrime(int n){
-        int c = 0;
-        if(n == 1){
-            return false;
-        }
-        for(int i=1; i<n; i++){
-            if(n % i == 0){
+            if(isPrime(arr[i]) && arr[i]<=k){
                 c++;
             }
         }
-        if(c == 1){
+
+        System.out.printf("%d",c);
+    }
+
+    public static boolean isPrime(int num){
+        if(num==1){
+            return false;
+        }
+        int c = 0;
+        for(int i=1; i<num; i++){
+            if(num%i==0){
+                c++;
+            }
+        }
+        if(c<2){
             return true;
         }
+
         return false;
     }
 }
