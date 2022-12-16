d6f487677b50628a6e7c1f7de7ed5c3639bb7826
Manchina <104674915+Manchina@users.noreply.github.com>
2022-12-16 20:40:47 +0530

    import java.util.Scanner; class Solution{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int c = sc.nextInt();         float f = (float)(c*1.8) + 32;         System.out.printf("%.2f",f);     } }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        float f = (float)(c*1.8) + 32;
+        System.out.printf("%.2f",f);
+    }
+}
