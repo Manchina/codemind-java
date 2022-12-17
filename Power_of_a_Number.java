18cc9d8cb68f7b9ace2f4e108cb3f2831b7ad125
Manchina <104674915+Manchina@users.noreply.github.com>
2022-12-17 08:07:49 +0530

    import java.util.*; class Solution{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int y = sc.nextInt();         int m = sc.nextInt();
            System.out.print((int)(Math.pow(x,y))%m);
        }
    }

---
+++ Power_of_a_Number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        
+        
+        
+        System.out.print((int)(Math.pow(x,y))%m);
+    }
+}
